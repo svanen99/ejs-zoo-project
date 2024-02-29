7c5557b72a00d083dd7eb8e61f2c0ece6ed444e7
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-02-29 17:53:13 +0100

    somewhere on the way to getting the content there

--- routes/animalRouter.js
+++ routes/animalRouter.js
@@ -1,30 +1,31 @@
 import express from 'express';
 import { animals } from '../data/animals.js';
-import { mammals } from '../data/animals.js';
-import { reptiles } from '../data/animals.js';
-import { birds } from '../data/animals.js';
+import { mammals, reptiles, birds } from '../data/animals.js';
 
 const animalRouter = express.Router();
 
-
 animalRouter.get('/:animalType', (req, res) => {
-    const animalType = req.params.animalType;
+    const animalName = req.params.animalType;
     let animal;
+
+    animal = mammals.find(animal => animal.name === animalName);
+    if (!animal) {
+        animal = reptiles.find(animal => animal.name === animalName);
+    }
+    if (!animal) {
+        animal = birds.find(animal => animal.name === animalName);
+    }
 
     if (animal) {
         res.render(
             'pages/animal-detailed',
             {
-                pageTitle: animalType,
+                pageTitle: animalName,
                 subTitle: "Here's some more info about this little fella",
                 className: "detailed-animal",
-                animals: {
-                    mammals: mammals,
-                    reptiles: reptiles,
-                    birds: birds,
-                    }
+                animals: animal
             }
-        )
+        );
     } else {
         res.status(404).send("Oops, animal not found");
     }
