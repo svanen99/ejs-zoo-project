64e24f07d007ab938bf46479b74aec509e238083
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-02-29 12:25:50 +0100

    sorting problem

--- routes/animalRouter.js
+++ routes/animalRouter.js
@@ -3,20 +3,21 @@ import { animals } from '../data/animals.js';
 
 const animalRouter = express.Router();
 
-animalRouter.get('/:animalType', (req, res) => {
+animalRouter.get('/?animalType', (req, res) => {
     const animalType = req.params.animalType;
 
     const animal = animals.find(animal => animal.name.toLowerCase() === animalType.toLowerCase());
 
     if (animal) {
-        res.render(
-            'pages/animal-details',
-            {
-                pageTitle: animalType,
-                subTitle: "Here's some more info about this little fella",
-                animal: animal
-            }
-        )
+        // res.render(
+        //     'pages/animal-details',
+        //     {
+        //         pageTitle: animalType,
+        //         subTitle: "Here's some more info about this little fella",
+        //         animal: animal
+        //     }
+        // )
+        res.send("animal found!")
     } else {
         res.status(404).send("Oops, animal not found");
     }
@@ -24,3 +25,4 @@ animalRouter.get('/:animalType', (req, res) => {
 
 export default animalRouter;
 console.log(animals);
+
