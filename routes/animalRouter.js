19cc176ec615c8afc5db2a857c43e6b60aa01bff
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-03-01 15:56:32 +0100

    trying to get hold of right content

--- routes/animalRouter.js
+++ routes/animalRouter.js
@@ -12,27 +12,45 @@ let animalName = "echidna";
 animalRouter.get('/:animalType', (req, res) => {
     const animalName = req.params.animalType;
     if (mammals.some(animal => animal.name === animalName)) { 
-        res.send("it's a mammal")
+        
+        res.render(
+            'pages/animal-detailed',
+            {
+                pageTitle: animalName,
+                subTitle: "Here's some more info about this little fella",
+                className: "detailed-animal",
+                animal: animalName,
+                animalGroup: mammals
+            }
+        );
+
     } else if (reptiles.some(animal => animal.name === animalName)) {
-        res.send("it's a reptile")
+        console.log("it's a reptile");
+
+        res.render(
+            'pages/animal-detailed',
+            {
+                pageTitle: animalName,
+                subTitle: "Here's some more info about this little fella",
+                className: "detailed-animal",
+                animal: animalName,
+                animalGroup: mammals,
+            }
+        );
+
     } else if (birds.some(animal => animal.name === animalName)) {
-        res.send("it's a bird")
+        console.log("it's a bird")
 
-
-    
-        // console.log("that animal exists!")
-        // res.send("Animal exists!")
-        // console.log(animalName);
-
-        // res.render(
-        //     'pages/animal-detailed',
-        //     {
-        //         pageTitle: animalName,
-        //         subTitle: "Here's some more info about this little fella",
-        //         className: "detailed-animal",
-        //         animal: animalName
-        //     }
-        // );
+        res.render(
+            'pages/animal-detailed',
+            {
+                pageTitle: animalName,
+                subTitle: "Here's some more info about this little fella",
+                className: "detailed-animal",
+                animal: animalName,
+                animalGroup: mammals,
+            }
+        );
     } else {
         res.status(404).send("Oops, animal not found");
     }
